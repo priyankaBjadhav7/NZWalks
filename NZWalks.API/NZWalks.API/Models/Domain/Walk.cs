1ecb15526cf9e75e5e8ee51f8469bbee593c8b0a
priyanka <priyankajadhav754@gmail.com>
2022-12-08 12:33:21 +0530

    Adding models and migration using code first approch-section 2

---
+++ NZWalks.API/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //navigation
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
